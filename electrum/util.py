baebb75ece6248caa8290156ff3ebfa7add845f9
pedro-at-decenomy <pedro@decenomy.net>
2022-01-04 12:58:54 +0000

    changes BITCOIN_BIP21_URI_SCHEME

--- electrum/util.py
+++ electrum/util.py
@@ -880,7 +880,7 @@ def block_explorer_URL(config: 'SimpleConfig', kind: str, item: str) -> Optional
 
 
 # note: when checking against these, use .lower() to support case-insensitivity
-BITCOIN_BIP21_URI_SCHEME = 'bitcoin'
+BITCOIN_BIP21_URI_SCHEME = 'sapphire'
 LIGHTNING_URI_SCHEME = 'lightning'
 
 
